0a5d8df8eb4077cf95d1312e11f29ef0200f41b9
BLooperZ <blooperz@users.noreply.github.com>
2022-09-18 20:42:10 +0300

    batch decode fonts

--- kyraim/moved/kyrafont/kyrafnt.py
+++ kyraim/moved/kyrafont/kyrafnt.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import math
+import os
+import pathlib
 import struct
 from image import save_image_grid
-import math
-import sys
+from kyraim.texts import match_archive_files
 
 
 def read_le_uint16(f):
@@ -18,80 +20,85 @@ def grouper(it, chunk_size):
     return zip(*([iter(it)] * chunk_size))
 
 
-filename = sys.argv[1]
-if len(sys.argv) < 2:
-    exit(1)
-
-with open(filename, 'rb') as fntFile:
-    data = fntFile.read()
-
-fileSize, fontSig, descOffset = struct.unpack('<3H', data[0:6])
-
-print((fileSize, fontSig, descOffset))
-if fontSig != 0x0500:
-    raise ValueError("DOSFont: invalid font: 0x{:04X})".format(fontSig))
-
-
-width = data[descOffset + 5]
-height = data[descOffset + 4]
-numGlyphs = data[descOffset + 3] + 1
-
-print((width, height, numGlyphs))
-
-bitmapOffsetsStart, _widthTableStart, unknown2, _heightTableStart = struct.unpack(
-    '<4H', data[6:14]
-)
-print((bitmapOffsetsStart, _widthTableStart, unknown2, _heightTableStart))
-bitmapOffsets = [
-    read_le_uint16(bytes(t))
-    for t in grouper(sublist(data, bitmapOffsetsStart, 2 * numGlyphs), 2)
-]
-widths = sublist(data, _widthTableStart, numGlyphs)
-heights = list(grouper(sublist(data, _heightTableStart, 2 * numGlyphs), 2))
-
-_colorMap = list(range(16))  # [x for x in data[unknow2:unknow2+16]]
-
-
 def flatten(ls):
     return (item for sublist in ls for item in sublist)
 
+_colorMap = list(range(16))
+palette = [(133 * x % 256) for x in range(256)] * 3
 
 def decode_line(bbs):
     return list(flatten((_colorMap[b % 16], _colorMap[b // 16]) for b in bbs))
 
 
-def convert_char(c):
-    charWidth = widths[c]
+def convert_char(height, chardata, cheights, cwidth):
+    charH1, charH2 = cheights
+    charH0 = height - sum(cheights)
 
-    charH1, charH2 = heights[c]
-    charH0 = height - sum(heights[c])
+    pic = [[_colorMap[0]] * cwidth] * charH1
 
-    pic = [[_colorMap[0]] * charWidth] * charH1
+    pic.append([255] * cwidth)
 
-    pic.append([255] * charWidth)
+    read_size = math.ceil(cwidth / 2)
+    chunked = grouper(chardata[:read_size * charH2], read_size)
+    pic += [decode_line(chunk)[:cwidth] for chunk in chunked]
 
-    read_size = math.ceil(charWidth / 2)
-    chunked = grouper(sublist(data, bitmapOffsets[c], read_size * charH2), read_size)
-    pic += [decode_line(chunk)[:charWidth] for chunk in chunked]
+    pic.append([255] * cwidth)
 
-    pic.append([255] * charWidth)
-
-    pic += [[_colorMap[0]] * charWidth] * charH0
+    pic += [[_colorMap[0]] * cwidth] * charH0
     return pic
 
 
-chars = (convert_char(c) for c in range(numGlyphs))
+def decode_font_file(filename, data):
+    path = pathlib.Path(filename)
+    stem = path.stem
 
-locs = [
-    {'x1': 0, 'y1': 0, 'x2': widths[c], 'y2': sum(heights[c])} for c in range(numGlyphs)
-]
+    fileSize, fontSig, descOffset = struct.unpack('<3H', data[0:6])
 
-palette = [(133 * x % 256) for x in range(256)] * 3
-save_image_grid('chars.png', chars, palette)
+    print((fileSize, fontSig, descOffset))
+    if fontSig != 0x0500:
+        print("DOSFont: invalid font {}: 0x{:04X}".format(filename, fontSig))
+        return
 
-# for t, m in heights:
-#     print((t, m , height - t - m))
+    assert len(data) == fileSize
+    assert descOffset == 14, descOffset
+    bitmapOffsetsStart, _widthTableStart, unknown2, _heightTableStart = struct.unpack(
+        '<4H', data[6:14]
+    )
+    print(data[descOffset:descOffset+2])
+    numGlyphs = data[descOffset + 3] + 1
+    height = data[descOffset + 4]
+    width = data[descOffset + 5]
 
-# for i in range(255):
-#     # print((i & 1 != 0) == (i % 2 == 1))
-#     print(((i & 0xF) % 256) == (i % 16))
+    print((width, height, numGlyphs))
+
+    print((bitmapOffsetsStart, _widthTableStart, unknown2, _heightTableStart))
+    bitmapOffsets = [
+        read_le_uint16(bytes(t))
+        for t in grouper(sublist(data, bitmapOffsetsStart, 2 * numGlyphs), 2)
+    ]
+    widths = sublist(data, _widthTableStart, numGlyphs)
+    heights = list(grouper(sublist(data, _heightTableStart, 2 * numGlyphs), 2))
+
+    assert len(bitmapOffsets) == len(widths) == len(heights) == numGlyphs, (len(bitmapOffsets), len(widths), len(heights), numGlyphs)
+
+    chars = (convert_char(height, data[offset:], cheights, cwidth) for offset, cheights, cwidth in zip(bitmapOffsets, heights, widths))
+
+    save_image_grid(f'{stem}.png', chars, palette)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='extract pak archive')
+    parser.add_argument('directory', help='files to extract')
+    args = parser.parse_args()
+
+    for pak, pattern, fname in match_archive_files(
+        args.directory, ['*.FNT']
+    ):
+        bname = os.path.basename(fname)
+        print(fname)
+        with pak.open(fname, 'rb') as stream:
+            data = stream.read()
+
+        decode_font_file(fname, data)
